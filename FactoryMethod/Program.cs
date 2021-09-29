f286200d20970cf8838e138897b49237a87c2a71
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-29 19:02:30 +0300

    Factory Method Design

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory());
+
+            CustomerManager customerManager2 = new CustomerManager(new LoggerFactory2());
+
+            customerManager.Save();
+            customerManager2.Save();
+            Console.ReadLine();
+        }
+    }
+
+    public class LoggerFactory : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory
+            return new OngLogger();
+        }
+    }
+
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory
+            return new OngLogger2();
+        }
+    }
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public class OngLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with OngLogger");
+        }
+    }
+
+    public class OngLogger2 : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with OngLogger2");
+        }
+    }
+
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
