86f08b696bc07d5a3f5b5d9944239016a87498d0
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-10-07 18:40:20 +0300

    Strategy Pattern

---
+++ Strategy/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(); customerManager.CreditCalculatorBase = new After2020CreditCalculator();
+            customerManager.SaveCredit();
+            customerManager.CreditCalculatorBase = new Before2020CreditCalculator();
+            customerManager.SaveCredit();
+
+            Console.ReadLine();
+
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class Before2020CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated using before2020");
+        }
+    }
+    class After2020CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated using after2020");
+        }
+    }
+
+    class CustomerManager
+    {
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+
+        public void SaveCredit()
+        {
+            Console.WriteLine("Customer manager business");
+            CreditCalculatorBase.Calculate();
+        }
+    }
+}
